a44a951759fe6b8138c7552ae31392c78e027cd6
Patrick GIRY <patrickgiry@gmail.com>
2017-01-07 22:01:10 +0100

    refactor(understanding parser combinators): change pChar to curried version

--- src/main/java/experiments/understanding/parser/combinators/Parser.java
+++ src/main/java/experiments/understanding/parser/combinators/Parser.java
@@ -1,20 +1,24 @@
 package experiments.understanding.parser.combinators;
 
+import java.util.function.Function;
+
 public class Parser {
 
     private Parser() {
     }
 
-    public static Result pChar(int match, String input) {
-        if (input != null && input.length() > 0) {
-            int first = input.codePointAt(0);
-            if (first == match) {
-                return Result.success(first, input.substring(1));
+    public static Function<String, Result> pChar(int match) {
+        return input -> {
+            if (input != null && input.length() > 0) {
+                int first = input.codePointAt(0);
+                if (first == match) {
+                    return Result.success(first, input.substring(1));
+                } else {
+                    return Result.failure("Expecting %c, got %c", match, first);
+                }
             } else {
-                return Result.failure("Expecting %c, got %c", match, first);
+                return Result.failure("No more input");
             }
-        } else {
-            return Result.failure("No more input");
-        }
+        };
     }
 }
--- src/test/java/experiments/understanding/parser/combinators/ParserTest.java
+++ src/test/java/experiments/understanding/parser/combinators/ParserTest.java
@@ -1,33 +1,45 @@
 package experiments.understanding.parser.combinators;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.function.Function;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ParserTest {
 
+    private Function<String, Result> parseA;
+
+    @Before
+    public void setUp() throws Exception {
+        parseA = Parser.pChar('A');
+
+    }
+
     @Test
     public void should_return_failure_with_null_remaining_when_input_is_null() throws Exception {
-        assertThat(Parser.pChar('A', null)).isEqualTo(Result.failure("No more input"));
+        assertThat(parseA.apply(null)).isEqualTo(Result.failure("No more input"));
     }
 
     @Test
     public void should_return_failure_with_empty_remaining_when_input_is_empty() throws Exception {
-        assertThat(Parser.pChar('A', "")).isEqualTo(Result.failure("No more input"));
+        assertThat(parseA.apply("")).isEqualTo(Result.failure("No more input"));
     }
 
     @Test
     public void should_return_success_with_remaining_when_input_start_by_A() throws Exception {
-        assertThat(Parser.pChar('A', "ABC")).isEqualTo(Result.success('A', "BC"));
+        assertThat(parseA.apply("ABC")).isEqualTo(Result.success('A', "BC"));
     }
 
     @Test
     public void should_return_success_with_remaining_when_input_start_by_E() throws Exception {
-        assertThat(Parser.pChar('E', "EBC")).isEqualTo(Result.success('E', "BC"));
+        Function<String, Result> parseE = Parser.pChar('E');
+        assertThat(parseE.apply("EBC")).isEqualTo(Result.success('E', "BC"));
     }
 
     @Test
     public void should_return_failure_with_input_as_remaining_when_input_not_start_by_A() throws Exception {
-        assertThat(Parser.pChar('A', "ZBC")).isEqualTo(Result.failure("Expecting A, got Z"));
+        assertThat(parseA.apply("ZBC")).isEqualTo(Result.failure("Expecting A, got Z"));
     }
 }
